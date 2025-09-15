6649620d188615ac462a9490827a3301a5d3bcb2
cewko <221901489+cewko@users.noreply.github.com>
2025-09-15 14:03:02 +0200

    implement authentication system

--- educa/settings.py
+++ educa/settings.py
@@ -124,3 +124,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/account/login/'
+LOGOUT_URL = '/account/logout/'
+LOGIN_REDIRECT_URL = '/'
