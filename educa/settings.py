1e292e785e39a98717335c79cbf29a1de776b96d
cewko <221901489+cewko@users.noreply.github.com>
2025-09-19 22:26:45 +0200

    fix typo in drf settings

--- educa/settings.py
+++ educa/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionOrAnonReadOnly",
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
     ]
 }
 
