3704cb1d549d1ce47412045279bf50f663eb6a48
cewko <221901489+cewko@users.noreply.github.com>
2025-09-20 16:40:44 +0200

    use enroll action in course viewset

--- courses/api/views.py
+++ courses/api/views.py
@@ -9,22 +9,33 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+# class CourseEnrollView(APIView):
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({"enrolled": True})
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({"enrolled": True})
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related("modules")
     serializer_class = CourseSerializer
-    pagination_class = StandardPagination
+
+    @action(
+        detail=True,
+        methods=["post"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated]
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({"enrolled": True})
 
 
 class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
