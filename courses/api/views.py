fecb2bd4559ae6d79aa1f863dae6a63613068151
cewko <221901489+cewko@users.noreply.github.com>
2025-09-20 14:42:21 +0200

    implement basic authentication and permissions

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,9 +7,14 @@ from courses.api.pagination import StandardPagination
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
