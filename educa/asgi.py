2f0b633ccafc932e77893810a06a16a93ce5df92
cewko <221901489+cewko@users.noreply.github.com>
2025-09-21 13:56:54 +0200

    route connections to chat consumer

--- educa/asgi.py
+++ educa/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
@@ -15,6 +16,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
 
 django_asgi_app = get_asgi_application()
 
+from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    )
 })
