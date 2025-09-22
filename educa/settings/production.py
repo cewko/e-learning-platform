f24b899842f201790b59ae5874ac585885cf471d
cewko <221901489+cewko@users.noreply.github.com>
2025-09-22 13:06:33 +0200

    use docker compose to orchestrate the containers

--- educa/settings/production.py
+++ educa/settings/production.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 from .base import *
 
 DEBUG = False
@@ -7,12 +8,16 @@ ADMINS = [
     ("Admin", os.environ.get('ADMIN_EMAIL', "admin@domain.com")),
 ]
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') if host.strip()]
 
 # Database configuration
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
